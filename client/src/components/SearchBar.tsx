7eb6fcd90f0199f9144ed2cbc32956e5496e60dd
Thierry-Lenepveu <thierry.lenepveu.33@gmail.com>
2024-11-18 16:09:53 +0100

    feat: implement component favorites

---
+++ client/src/components/SearchBar.tsx
@@ -0,0 +1,10 @@
+function SearchBar() {
+  return (
+    <div id="search-bar">
+      <img src="/src/assets/search.png" alt="" />
+      <input type="text" className="search-input" placeholder="Rechercher..." />
+    </div>
+  );
+}
+
+export default SearchBar;
