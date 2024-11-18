afbc908b47572a7291800472c8813ddc248c3349
abdoukhoudoss <cisse.abdoukhoudoss@gmail.com>
2024-11-18 17:08:06 +0100

    fix: add icons for pages and logo

--- client/src/components/Header.tsx
+++ client/src/components/Header.tsx
@@ -4,9 +4,23 @@ function Header() {
   return (
     <>
       <nav>
-        <Link to="/">Accueil</Link>
-        <Link to="FiveDays">5jours</Link>
-        <Link to="Favorites">Favoris</Link>
+        <div className="logo">
+          <img src="./src/assets/images/logo.png" alt="weatherly-logo" />
+        </div>
+        <div className="icons">
+          <Link to="/">
+            <img src="./src/assets/images/today.png" alt="today-weather" />
+            <p>Accueil</p>
+          </Link>
+          <Link to="FiveDays">
+            <img src="./src/assets/images/five-days.png" alt="five-days" />
+            <p>5 jours</p>
+          </Link>
+          <Link to="Favorites">
+            <img src="./src/assets/images/Favoris.png" alt="favoris" />
+            <p>Favoris</p>
+          </Link>
+        </div>
       </nav>
     </>
   );
