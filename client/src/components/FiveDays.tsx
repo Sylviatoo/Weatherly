d2450a2b9b63914c2e96a52c94789387fe0e58b7
Helwena <helwena8@gmail.com>
2024-11-21 15:09:08 +0100

    style: add five days style and media queries

--- client/src/components/FiveDays.tsx
+++ client/src/components/FiveDays.tsx
@@ -3,49 +3,50 @@ import img6 from "../assets/images/6.png";
 import img13 from "../assets/images/13.png";
 import img17 from "../assets/images/17.png";
 import img18 from "../assets/images/18.png";
+import "../style-css/FiveDaysStyle.css";
 
 function FiveDays() {
   const daysArray = [
     {
       id: "01",
       img: img1,
-      date: "Lundi 18 novembre",
+      date: "Lundi 18 nov.",
       degree: "13°c",
     },
 
     {
       id: "02",
       img: img17,
-      date: "Mardi 19 novembre",
+      date: "Mardi 19 nov.",
       degree: "16°c",
     },
 
     {
       id: "03",
       img: img18,
-      date: "Mercredi 20 novembre",
+      date: "Mercredi 20 nov.",
       degree: "18°c",
     },
     {
       id: "04",
       img: img13,
-      date: "Jeudi 20 novembre",
+      date: "Jeudi 20 nov.",
       degree: "17°c",
     },
     {
       id: "05",
       img: img6,
-      date: "Vendredi 21 novembre",
+      date: "Vendredi 21 nov.",
       degree: "20°c",
     },
   ];
 
   const daysArrayDisplay = daysArray.map((day) => {
     return (
-      <div key={day.id}>
-        <img src={day.img} alt={`Météo ${day.date}`} />
-        <p>{day.date}</p>
-        <p>{day.degree}</p>
+      <div key={day.id} className="day-box">
+        <img src={day.img} alt={`Météo ${day.date}`} className="img-icons" />
+        <p className="date">{day.date}</p>
+        <p className="degree">{day.degree}</p>
       </div>
     );
   });
@@ -53,14 +54,14 @@ function FiveDays() {
   return (
     <div>
       <Days />
-      <div>{daysArrayDisplay}</div>
+      <div className="container">{daysArrayDisplay}</div>
     </div>
   );
 }
 
 function Days() {
   return (
-    <div>
+    <div className="days-city">
       <h2 className="days">5 jours</h2>
       <h3 className="city">Toulouse</h3>
     </div>
